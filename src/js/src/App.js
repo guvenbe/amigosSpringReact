068265b915f0bceeecc877f86e8bc3bfda7b97d5
Bora Guven <guvenbe@gmail.com>
2019-08-24 02:51:30 -0400

    Added Hello World

---
+++ src/js/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <h1>Hello worls Spring Boot and React</h1>;
+  }
+}
+
+export default App;
