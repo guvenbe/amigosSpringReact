8947da2cbf03fd6ebafc5d1b8fb39533352e73a7
Bora Guven <guvenbe@gmail.com>
2019-08-24 13:52:13 -0400

    Added Ant Table

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -18,33 +18,67 @@ class App extends Component {
         getAllStudents()
             .then(res => res.json()
                 .then(students => {
-                    console.log(students)
+                    console.log(students);
                     this.setState({students});
 
                 }));
     };
 
     render() {
-        const { students } =this.state;
+        const {students} = this.state;
 
-        if (students && students.length){
-            return students.map((student, index) => {
-                    return(
-                        <div key={index}>
-                            <h2>{student.studentId}</h2>
-                            <h2>{student.firstName}</h2>
-                            <h2>{student.lastName}</h2>
-                            <h2>{student.gender}</h2>
-                            <h2>{student.email}</h2>
-                        </div>
-                    );
-                }
+        //****************Below is without antd
+        /*        if (students && students.length){
+                    return students.map((student, index) => {
+                            return(
+                                <div key={index}>
+                                    <h2>{student.studentId}</h2>
+                                    <h2>{student.firstName}</h2>
+                                    <h2>{student.lastName}</h2>
+                                    <h2>{student.gender}</h2>
+                                    <h2>{student.email}</h2>
+                                </div>
+                            );
+                        }
 
-            )
-        }
-        return <h1>No Students found students</h1>
+                    )
+                }*/
 
+        const columns = [
+            {
+                title: 'StudentId',
+                dataIndex: 'StudentId',
+                key: 'StudentId'
+            },
+            {
+                title: 'First Name',
+                dataIndex: 'firstName',
+                key: 'firstName'
+            },
+            {
+                title: 'Last Name',
+                dataIndex: 'lastName',
+                key: 'lastName'
+            },
+            {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email'
+            }, {
+                title: 'Gender',
+                dataIndex: 'gender',
+                key: 'gender'
+            },
 
+        ];
+        return (
+            <Table
+                dataSource={students}
+                columns={columns}
+                rowKey='studentId'
+            />
+
+        );
 
 
     }
