30542e1a36024bf3bad419504380c3af101d9fec
Bora Guven <guvenbe@gmail.com>
2019-08-24 11:42:56 -0400

    Created Initial react front end Checked Creted JSX and call REST spring from JSX Added antd to package.jsom

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,11 +1,53 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, {Component} from "react";
 import "./App.css";
+import {getAllStudents} from "./client";
+import {Table} from 'antd';
+
 
 class App extends Component {
-  render() {
-    return <h1>Hello worls Spring Boot and React</h1>;
-  }
+
+    state = {
+        students: []
+    };
+
+    componentDidMount() {
+        this.fetchStudents();
+    }
+
+    fetchStudents = () => {
+        getAllStudents()
+            .then(res => res.json()
+                .then(students => {
+                    console.log(students)
+                    this.setState({students});
+
+                }));
+    };
+
+    render() {
+        const { students } =this.state;
+
+        if (students && students.length){
+            return students.map((student, index) => {
+                    return(
+                        <div key={index}>
+                            <h2>{student.studentId}</h2>
+                            <h2>{student.firstName}</h2>
+                            <h2>{student.lastName}</h2>
+                            <h2>{student.gender}</h2>
+                            <h2>{student.email}</h2>
+                        </div>
+                    );
+                }
+
+            )
+        }
+        return <h1>No Students found students</h1>
+
+
+
+
+    }
 }
 
 export default App;
